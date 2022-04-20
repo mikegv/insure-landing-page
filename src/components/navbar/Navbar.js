053feef4b974f1c9137cc08657bacab2b9fd7406
Mikegv <xmik3yvx@live.com>
2022-04-20 14:11:11 -0700

    feat(navbar):completed navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,13 +4,15 @@ import './Navbar.css'
 class Navbar extends Component {
     render() {
         return (
-            <div>
-                NAVBAR How we work
-  Blog
-  Account
-  View plans
+            <div className='nav'>
+                <img src={process.env.PUBLIC_URL + '/images/logo.svg'} alt='insure logo' />
+                <ul>
+                    <li><a href="#">HOW WE WORK</a></li>
+                    <li><a href="#">BLOG</a></li>
+                    <li><a href="#">ACCOUNT</a></li>
+                    <li><a href="#" className='boxLink'>VIEW PLANS</a></li>
+                </ul>
 
-  Navbar
             </div>
         );
     }
