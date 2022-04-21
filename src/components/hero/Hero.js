010b24819b3c902e3d2d0853a736a05bbf86a209
Mikegv <xmik3yvx@live.com>
2022-04-21 15:44:02 -0700

    feat(hero):hero section is completed

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,23 +4,29 @@ import './Hero.css'
 const Hero = () => {
     return (
         <div className='hero'>
+            <img src={process.env.PUBLIC_URL + '/images/bg-pattern-intro-right-desktop.svg'} alt='intro desktop bg' className='bgRightPattern' />
+            <img src={process.env.PUBLIC_URL + '/images/bg-pattern-intro-left-desktop.svg'} alt='intro desktop bg' className='bgLeftPattern' />
+
             <div className='heroContent'>
                 <div className='heroLeft'>
-                    <hr />
-                    <p>Humanizing <br /> your insurance.</p>
+                    <hr className='heroTopHr'/>
+                    <p className='heroTitle'>Humanizing <br /> your insurance.</p>
 
-                    Get your life insurance coverage easier and faster. We blend our expertise
-                    and technology to help you find the plan that’s right for you. Ensure you
-                    and your loved ones are protected.
+                    <p>Get your life insurance coverage easier and faster. We blend our expertise
+                        and technology to help you find the plan that’s right for you. Ensure you
+                        and your loved ones are protected.
+                    </p>
+                    <br />
+                    <a href="#" className='heroBoxLink'>View plans</a>
 
-                    View plans
                 </div>
                 <div className='heroRight'>
                     <img src={process.env.PUBLIC_URL + '/images/image-intro-desktop.jpg'} alt='intro desktop' />
-
-                    Hero
                 </div>
+                <hr className='bottomHeroHr'/>
+
             </div>
+
         </div>
     );
 };
